b883b5ed35181f3af62d3d1998cb8c43001b3835
Jon Bake <jonmbake@gmail.com>
2014-06-20 19:56:08 -0500

    Clean up code a bit

--- library/sendmail.php
+++ library/sendmail.php
@@ -45,7 +45,7 @@
   header('Content-type: application/json');
   //do some simple validation. this should have been validated on the client-side also
   if (empty($email)) {
-  	errorResponse('Email or message is empty.');
+    errorResponse('Email or message is empty.');
   }
 
   //do Captcha check, make sure the submitter is not a robot:)...
@@ -57,9 +57,8 @@
 
   //try to send the message
   if(mail(MY_EMAIL, EMAIL_SUBJECT, setMessageBody($fields_req), "From: $email")) {
-  	echo json_encode(array('message' => 'Your message was successfully submitted.'));
+    echo json_encode(array('message' => 'Your message was successfully submitted.'));
   } else {
-  	header('HTTP/1.1 500 Internal Server Error');
-  	echo json_encode(array('message' => 'Unexpected error while attempting to send e-mail.'));
+    errorResponse('Unexpected error while attempting to send e-mail.');
   }
 ?>
